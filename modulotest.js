4f9c98e436b896fef3f971ea29efe9ea0c201355
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-01 12:38:08 -0400

    exists

---
+++ modulotest.js
@@ -0,0 +1,15 @@
+function moduligma(num1, num2) {
+    let result = (num1 % num2)
+    return (result)
+}
+
+
+let numOne = 10;
+let numTwo = 5;
+
+console.log(moduligma(numOne, numTwo))
+
+let numA = 20;
+let numB = 105;
+
+console.log(moduligma(numB, numA))
