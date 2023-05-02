6bf7ff46cb01493487c9202d687b1ccbea137002
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-02 11:29:24 -0400

    made a cool new inverted removenumber

--- removeletterforresume.js
+++ removeletterforresume.js
@@ -9,6 +9,18 @@ function removeletters2() {
             outputnumber.push(currentCharacter)
         }
     }
-    console.log(document.getElementById("ligma").innerHTMLNumber = (outputnumber.join("")))
+    document.getElementById("ligma").innerHTML = outputnumber.join("")
+}
+function removenumbers2() {
+    let inputstring = document.getElementById('myText').value
+    let outputnumber = []
+    let noombers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+    for (let i = 0; i < inputstring.length; i++) {
+        let currentCharacter = inputstring[i]
+        if (noombers.includes(currentCharacter) == false) {
+            outputnumber.push(currentCharacter)
+        }
+    }
     document.getElementById("ligma").innerHTML = outputnumber.join("")
 }
