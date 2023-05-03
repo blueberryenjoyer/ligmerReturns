f579767a0406a21cc10d539a97fc877732ef32dd
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-03 12:07:48 -0400

    chess rating project

---
+++ bethharmon.js
@@ -0,0 +1,19 @@
+function titleRanking(rating) {
+    if (rating < 1201) {
+        return "Beginner"
+    }
+    else if (rating < 1801) {
+        return "Intermediate"
+    }
+    else if (rating < 2201) {
+        return "Intermediate"
+    }
+    else if (rating === 2852) {
+        return "Magnus F*cking Carlsen"
+    }
+    else {
+        return "Master"
+    }
+}
+
+console.log(titleRanking(3000))
