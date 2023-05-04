c6f98e62ffed408654c2fe53a6b1741b71499fea
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-04 14:04:24 -0400

    made vault problem answer

---
+++ vaults.js
@@ -0,0 +1,16 @@
+// JavaScript creates so much more functionality for a site and automates processes.
+//You are building a vault that requires three mathematical calculations to generate the three codes in a combination.
+//Use the JavaScript console or the script block within an HTML page to create three variables.Each variable will be the result of each calculation(three in total).
+//The combination of the lock is 10 - 40 - 39. You must use three different arithmetic operators that will generate each individual number, 
+//then display the combination on the HTML page, or in an alert popup.
+
+let var1 = 15 - 5
+let var2 = 10 * 4
+let var3 = 78 / 2
+let halfString = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+console.log(`${halfString} ${var1} - ${var2} - ${var3}`)
+
+//why do i need comments to explain string concatenation? im literally doing one operation.
+//like yeah so my plan is to make the variables, and then add them together.
+//what am i supposed to explain, the syntax? i didnt invent the syntax
+
