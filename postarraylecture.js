a42d66bba85136dfc25a0b8be1f514ec3fa075e0
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-04 12:33:39 -0400

    ligma balls

---
+++ postarraylecture.js
@@ -0,0 +1,42 @@
+// Write a function to check whether an `input` is an array or not.
+// - write a function
+// - it takes one parameter
+// - the function should return true if the input is an array, and false if it is not an array
+//   - Test data: console.log(is_array('w3resource')); //false
+//   - Test data: console.log(is_array([1, 2, 4, 0]));  //true
+
+
+
+function arrayChecker(input) {
+    if (Array.isArray(input) === true) {
+        return true
+    }
+    else { return false }
+}
+
+console.log(arrayChecker([1289038, 12383]))
+
+// Write a function to get a specific element of an array. 
+// - write a function
+// - it will take two parameters, the first is an array, the second is the index that you are looking for
+// - the function should return the element of the array at the specified index
+// - do not mutate the original array
+
+
+function getElement(input, input2) {
+    return input[input2]
+}
+
+console.log(getElement([1, 2, 3, 4], 1))
+console.log(['asdfaf', '3444444', 3, "i hate the antichrist"][1]) //lmao its the same
+
+// Write a function that will add the first and last numbers in an array
+// - write a function
+// - it will take one parameter that is an array of numbers
+// - return the sum of the first and last numbers of the array 
+
+function brok(arr) {
+    return (arr[0] + arr[arr.length - 1])
+}
+
+console.log(brok([0, 1, 2, 3]))
