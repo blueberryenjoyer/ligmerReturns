88ce5635c400ae62401c4c3be7dfc0c341fef713
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-08 11:22:16 -0400

    first practice problem

---
+++ week3/practiceforloops1.js
@@ -0,0 +1,14 @@
+// Print all numbers down to 0
+// - create a function
+// - it should take one parameter, which represents a number
+// - the function should print all the numbers from the parameter down to, and including 0
+
+
+
+function brotherPleaseShareWithMeSomeLoops(nubmenr) {
+    for (let i = nubmenr; i >= 0; i--) {
+        console.log(i)
+    }
+}
+
+brotherPleaseShareWithMeSomeLoops(20)
