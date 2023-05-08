29e17c347e43c99cfc8b03daeb24837433e5b3da
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-08 11:42:18 -0400

    the holy grail of difficult practice problems

---
+++ week3/practiceforloops2.js
@@ -0,0 +1,40 @@
+// Print all odd numbers less than 100
+// - create a function
+// - print all odd numbers less than 100
+
+function allOdds() {
+    console.log("all odd numbers less than 100")
+}
+
+//i technically did it, LOL
+
+function evenOrOddSmaller(numberValue) { //old function
+    let newerint = Math.floor(numberValue / 2)
+    if (newerint === numberValue / 2) { return false } //this is even
+    else { return true } //this is odd
+}
+
+
+function correctOdds(input) {
+    if (input == "please") {
+
+        let output = []
+
+        for (let solution = 0; solution <= 100;) {
+            if (evenOrOddSmaller(solution) === true) {
+                output.push(solution)
+            }
+            solution++
+        }
+        return output
+
+
+
+
+    }
+    else {
+        return "you gotta say the magic word!"
+    }
+}
+
+console.log(correctOdds("please"))
