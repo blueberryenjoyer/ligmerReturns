98edc4f4ceebcf1feb448c7fdab5c14c83873d56
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-11 12:40:57 -0400

    way beyond the sea, there is a land with no war

---
+++ week3/ascelad.js
@@ -0,0 +1,32 @@
+/*
+#2: 
+Prompt: Each person in this pairing should create an object that describes the main character of that person's favorite tv show or movie. Each object should contain at least 5
+key-value pairs (although please feel free to include more if you like.) Those key-value pairs should have keys called:
+    - name (which you can expect will be given a string value)
+    - age (which you can expect will be given an integer value)
+    - occupations (which you can expect will be given an array of strings, where each string represents an occupation)
+    - isHero (which you can expect will be a boolean) 
+
+Lastly, you should write 8 console.log statements. The first 4 should console.log each individual key-value pair using bracket notation, and the latter 4 should console.log
+each key-value pair using dot notation. 
+
+At the end of this exercise, each of you should have 2 objects, one for each of you. 
+*/
+
+const ascelad = {
+    name: 'ascelad',
+    age: 50, //im roughly guessing here. could be 40 or 60
+    occupations: ['war', 'raid things', 'crimes against humanity', 'make fun of the french language'],
+    isHero: false
+}
+
+//console.log(Object.entries(ascelad))
+
+console.log(ascelad.name)
+console.log(ascelad.age)
+console.log(ascelad.occupations)
+console.log(ascelad.isHero)
+console.log(ascelad['name'])
+console.log(ascelad['age'])
+console.log(ascelad['occupations'])
+console.log(ascelad['isHero'])
