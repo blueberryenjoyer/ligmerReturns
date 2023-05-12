c5ead34f0c198a44c832c10c453fdc1c3d36b6f0
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-12 12:35:42 -0400

    i am rapidly sustaining psychological damage whyw hwy whyw hwy hwy hywy w hwyw hwy w why w hy w hyw  why w hwy why why whyw why why why w hyw hwy  why w hyw why wh y whwy why WHY IS "0" TRUTHY IF IT IS EQUAL TO FALSE AND 0 GOD PLEASE SMITE THIS CURSED TYPE COERCING DEVIL FROM THE WEB TURN BACK THE CLOCK AND UNDO THE LOOSE EQUALITY OPERATOR

---
+++ aaaligmer/test.js
@@ -0,0 +1,12 @@
+if ("0") { console.log("'0' is truthy'") } else { console.log("'0' is falsey") }
+if ("0" == false) { console.log("'0' is equal to false, and equal to 0") } //WHY? WHYYYYY? I HATE ACINQUAEFNUETN CN CNAT WAGHY
+//CAN YOU HEAR THE SILENCE
+//CAN YOU SEE THE DARK
+//CAN YOU HEAL THE HELPLESS
+//WELL IM BEGGING ON MY KNEES
+
+//javascript can you please explain to me why "0" is equal to false, but is not falsey?
+//what even in the fuck is going on?
+//why are your types fiction and equalities contrived and your booleans decided by the whim of a dead horse's neigh?
+//what could possibly determine the choices made by the developer of this language in that  "0" == false == 0 == "0" but apparently "0" is falsey
+//WHAT AND WHY
