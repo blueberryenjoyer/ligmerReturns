51d518b7d92c676b0481890f9bc2e4d63042aa76
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-15 15:21:21 -0400

    just more test changes to update with

--- aaaligmer/test.js
+++ aaaligmer/test.js
@@ -1,5 +1,5 @@
-if ("0") { console.log("'0' is truthy'") } else { console.log("'0' is falsey") }
-if ("0" == false) { console.log("'0' is equal to false, and equal to 0") } //WHY? WHYYYYY? I HATE ACINQUAEFNUETN CN CNAT WAGHY
+// if ("0") { console.log("'0' is truthy'") } else { console.log("'0' is falsey") }
+// if ("0" == false) { console.log("'0' is equal to false, and equal to 0") } //WHY? WHYYYYY? I HATE ACINQUAEFNUETN CN CNAT WAGHY
 //CAN YOU HEAR THE SILENCE
 //CAN YOU SEE THE DARK
 //CAN YOU HEAL THE HELPLESS
@@ -10,3 +10,20 @@ if ("0" == false) { console.log("'0' is equal to false, and equal to 0") } //WHY
 //why are your types fiction and equalities contrived and your booleans decided by the whim of a dead horse's neigh?
 //what could possibly determine the choices made by the developer of this language in that  "0" == false == 0 == "0" but apparently "0" is falsey
 //WHAT AND WHY
+
+// let arrr = [0, 1, 2, 3, 4]
+// console.log(arrr)
+// let ar2 = arrr.slice(0, 1)
+// console.log(ar2)
+// console.log(arrr)
+// let ar3 = arrr.splice(0, 1)
+// console.log(ar3)
+// console.log(arrr)
+
+//splice changes the original array
+//slice does not
+//thats fucked up because it means you can fuck up another array while just setting up a new one
+let aaa = [1, 2, 3]
+let bbb = [1, 2, 3]
+let arr = [...aaa, ...bbb]
+console.log(arr)
