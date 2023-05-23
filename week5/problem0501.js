1facd2a577a5d07c9ea56cd4898db2a9b1949399
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-05-23 13:01:13 -0400

    hunger games problem

---
+++ week5/problem0501.js
@@ -0,0 +1,35 @@
+// Prompt #1:
+// In "the Hunger Games", there are 12 districts, each of which send a tribute to fight in the games. As the capital's resident web developer, you've been given the job of writing a program that can automatically read out the names of all hunger games contestants.
+
+// Write a function called declareTributes that accepts one parameter called listOfTributes, which will be an array of objects. That parameter could receive an argument value like this: 
+
+let listOfTributes = [
+    {
+        name: "Katniss Everdeen",
+        district: 12
+    },
+    {
+        name: "Cato",
+        district: 2
+    }
+]
+
+// Your function should output a new array, where each element is a string written in this format:
+
+// [
+//   "Katniss Everdeen from District 12",
+//   "Cato from District 2"
+// ]
+
+// Use one of the array looping methods to solve this puzzle
+
+function changeFormat(input) {
+    return `${input.name} from District ${input.district}` //this return works
+}
+
+function declareTributes(inputTributes) {
+    inputTributes.forEach(changeFormat)
+    return inputTributes
+}
+
+console.log(declareTributes(listOfTributes))
